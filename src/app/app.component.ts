0ad4773e0c240ac1ab84465d8bfd5a7b40b75063
Vinicius Godinho <68762829+Vinicius147@users.noreply.github.com>
2024-02-24 23:11:33 -0300

    Create base components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuBarComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
